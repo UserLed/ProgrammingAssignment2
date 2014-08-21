ef978227e6a06bfaebc447ccb3246e4ab837e8aa
RWC <RW@RWs-MacBook-Air.local>
2014-08-21 20:33:14 +0800

    completed assignment 2
    RWC

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make a matrix, set initial value to null and set the matrix into cache
 
 makeCacheMatrix <- function(x = matrix()) {
+     i <- NULL
+               set <- function(y) {
+                x <<- y
+                i <<- NULL
+                }
 
+        get <-function(){ x }
+        setinv <- function(solve) {i <<- solve}
+        
+        getinv <- function() {i}
+        list(set = set, get = get, 
+                setinv = setinv,
+                getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## solve matrix using stored cache value if available, otherwise solve if NULL 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+                i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
+        
 }
